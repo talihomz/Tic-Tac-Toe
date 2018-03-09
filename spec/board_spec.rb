9961faf26d8d73b5c83187e7b7bea7b12a54f3d3
Kevin Wahome <wahomekevin@gmail.com>
2018-03-09 13:01:39 +0300

    Added Possible Wins test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,26 +2,44 @@ require 'board'
 
 describe Board do
 
-  subject { Board.new }
+  subject("board") { Board.new }
+  subject("winning_slots") {
+    [
+      [0,1,0,0,1,0,0,1,0],
+      [1,0,0,1,0,0,1,0,0],
+      [0,0,1,0,0,1,0,0,1],
+      [1,1,1,0,0,0,0,0,0],
+      [0,0,0,1,1,1,0,0,0],
+      [0,0,0,0,0,0,1,1,1],
+      [1,0,0,0,1,0,0,0,1],
+      [0,0,1,0,1,0,1,0,0]
+    ]
+  }
 
   describe "#check_win" do
-    context "given winning slots" do
-      it "returns true" do
-        subject.reset
-        subject.instance_variable_set(:@slots, [0,1,0,0,1,0,0,1,0])
 
-        expect(subject.check_win).to eq(true)
+    shared_examples "win_example" do |parameter|
+      it "Slot at #{parameter} returns true" do
+        board.reset
+        board.instance_variable_set(:@slots, winning_slots[parameter])
+        expect(board.check_win).to eq(true)
       end
     end
 
-    # context "given winning slots" do
-    #   it "returns true" do
-    #     subject.reset
-    #     subject.instance_variable_set(:@slots, [0,1,0,0,1,0,0,1,0])
-    #
-    #     expect(subject.check_win).to eq(true)
-    #   end
-    # end
+    context "given a collection of winning slots" do
+      8.times do |index|
+        include_examples "win_example", index
+      end
+      
+    end
+
+    context "given non winning slots" do
+      it "returns false" do
+        board.reset
+        
+        expect(board.check_win).to eq(false)
+      end
+    end
   end
 
 end
